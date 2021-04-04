b82d8bbccb1b2c67d390761c8959477a586b0e40
Wes Howard <BigRonald42@hotmail.com>
2021-04-04 13:48:36 -0700

    starting homepage get route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,7 +2,29 @@ const router = require('express').Router();
 
 router.get('/', async (req, res) => {
   // Send the rendered Handlebars.js template back as the response
-  res.render('homepage');
+  const posts = router.findAll({
+    attributes: [
+      'id',
+      'title',
+      'content',
+      'created-at'
+
+    ],
+    include: [{
+      model: User,
+      attributes: 'name'
+    },
+  {
+    model: Comment,
+    attributes: ['id', 'content', 'post_id', 'user_id', 'created_at'],
+    include: {
+      model: User,
+      attributes: 'name'
+    }
+
+  }]
+  })
+  res.render('homepage', posts);
 });
 
 module.exports = router;
