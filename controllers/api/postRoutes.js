46d4e869308f897ee34613d0da80a31ce28975b5
Wes Howard <BigRonald42@hotmail.com>
2021-04-07 09:06:54 -0700

    starting post routes & handlebars

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -5,35 +5,35 @@ const withAuth = require('../../utils/auth');
 
 // GET api/posts/ -- get all posts and include id, content, title, and created_at attributes
 router.get('/', async (req, res) => {
-    try{
-   const postData = await Post.findAll({
-      
-        attributes: [
-            'id',
-            'content',
-            'title',
-            'created_at',
-          ],
-        // post will be sorted from most recent to least recent
-        order: [[ 'created_at', 'DESC']],
-        //include post username
-        include: [
-            {
-                model: User,
-                attributes: ['username']
-            },
-            {
-                model: Comment,
-                attributes: ['id', 'content', 'post_id', 'user_id', 'created_at'],
-                include: {
+    try {
+        const postData = await Post.findAll({
+
+            attributes: [
+                'id',
+                'content',
+                'title',
+                'created_at',
+            ],
+            // post will be sorted from most recent to least recent
+            order: [['created_at', 'DESC']],
+            //include post username
+            include: [
+                {
                     model: User,
                     attributes: ['username']
+                },
+                {
+                    model: Comment,
+                    attributes: ['id', 'content', 'post_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
                 }
-            }
-        ]
-    })
-    .res.json(postData)
-}catch(err) {
+            ]
+        })
+            .res.json(postData)
+    } catch (err) {
         console.log(err);
         res.status(500).json(err);
     };
@@ -41,42 +41,57 @@ router.get('/', async (req, res) => {
 
 // GET api/posts/:id -- target a post by id and display the id, content, title, and created_at attributes.. also include all comments for that post and username that created it
 router.get('/:id', async (req, res) => {
-    try{
-    const postData = Post.findOne({
-      where: {
-        id: req.params.id
-      },
-     
-      attributes: [
-        'id',
-        'content',
-        'title',
-        'created_at',
-      ],
-      include: [
-        {
-          model: User,
-          attributes: ['username']
-        },
-        {
-            model: Comment,
-            attributes: ['id', 'content', 'post_id', 'user_id', 'created_at'],
-            include: {
-                model: User,
-                attributes: ['username']
-            }
-        }
-      ]
-    })
-     
+    try {
+        const postData = Post.findOne({
+            where: {
+                id: req.params.id
+            },
+
+            attributes: [
+                'id',
+                'content',
+                'title',
+                'created_at',
+            ],
+            include: [
+                {
+                    model: User,
+                    attributes: ['username']
+                },
+                {
+                    model: Comment,
+                    attributes: ['id', 'content', 'post_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                }
+            ]
+        })
+
         if (!postData) {
-          res.status(404).json({ message: 'ID not found' });
-          return;
+            res.status(404).json({ message: 'ID not found' });
+            return;
         }
         res.json(postData);
-      
-    }catch(err){
+
+    } catch (err) {
         console.log(err);
         res.status(500).json(err);
-      }
-  });
+    }
+});
+
+router.post('/', withAuth, async (req, res) => {
+    // expects {title: 'String of Stuff', content: 'String of stuff, all the text content.', user_id: 1}
+    try {
+        const postData = Post.create({
+            title: req.body.title,
+            content: req.body.content,
+            user_id: req.session.user_id
+        })
+        res.json(postData)
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
