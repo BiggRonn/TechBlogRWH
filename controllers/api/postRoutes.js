2cf578e469f514ffd5de1f2bb6cd09bed8a51456
Wes Howard <BigRonald42@hotmail.com>
2021-04-06 19:48:24 -0700

    added get post by id and commented

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -3,12 +3,11 @@ const { User, Post, Comment } = require('../../models');
 const sequelize = require('../../config/connection');
 const withAuth = require('../../utils/auth');
 
-// GET api/posts/ -- get all posts
+// GET api/posts/ -- get all posts and include id, content, title, and created_at attributes
 router.get('/', async (req, res) => {
     try{
    const postData = await Post.findAll({
-        // Query configuration
-        // From the Post table, include the post ID, URL, title, and the timestamp from post creation
+      
         attributes: [
             'id',
             'content',
@@ -39,3 +38,45 @@ router.get('/', async (req, res) => {
         res.status(500).json(err);
     };
 });
+
+// GET api/posts/:id -- get a single post by id with id, content, title, and created_at attributes.. also include all comments for that post and username that created it
+router.get('/:id', async (req, res) => {
+    try{
+    const postData = Post.findOne({
+      where: {
+        id: req.params.id
+      },
+     
+      attributes: [
+        'id',
+        'content',
+        'title',
+        'created_at',
+      ],
+      include: [
+        {
+          model: User,
+          attributes: ['username']
+        },
+        {
+            model: Comment,
+            attributes: ['id', 'content', 'post_id', 'user_id', 'created_at'],
+            include: {
+                model: User,
+                attributes: ['username']
+            }
+        }
+      ]
+    })
+     
+        if (!postData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(postData);
+      
+    }catch(err){
+        console.log(err);
+        res.status(500).json(err);
+      }
+  });
