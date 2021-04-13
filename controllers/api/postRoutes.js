ed83f371a4bb4d05cd30c61a6922dcec16449cd7
Wes Howard <BigRonald42@hotmail.com>
2021-04-13 09:24:57 -0700

    Updated readme, adding delete route and function

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -97,4 +97,24 @@ router.post('/', withAuth, async (req, res) => {
     }
 });
 
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+      const postData = await Post.destroy({
+        where: {
+          id: req.params.id,
+          user_id: req.session.user_id,
+        },
+      });
+  
+      if (!postData) {
+        res.status(404).json({ message: 'No post exists with that id' });
+        return;
+      }
+  
+      res.status(200).json(postData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
 module.exports = router;
